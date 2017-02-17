95961e3c7c9cac304c05ed560130567b74c5bb10
Dileep <priyauthram@yahoo.co.in>
2017-02-17 11:14:00 +1030

    Initial Commit

---
+++ MobileQuake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileQuake
+//
+//  Created by Dileep on 17/02/2017.
+//  Copyright © 2017 Priyanka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
